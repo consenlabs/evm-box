0dfc9ac0b9462c3e1364eddc1dc48637ccd7c00c
unix <unix.bio@gmail.com>
2021-08-02 10:18:48 +0800

    chore: add analzer env to build

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
+const withPlugins = require('next-compose-plugins')
 const { withSentryConfig } = require('@sentry/nextjs')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-const moduleExports = {
-  // Your existing module.exports
-  // disable Sentry CLI automatically detect and upload your source maps
-  sentry: {
-    disableServerWebpackPlugin: true,
-    disableClientWebpackPlugin: true,
+module.exports = withPlugins(
+  [
+    withSentryConfig({
+      sentry: {
+        disableServerWebpackPlugin: true,
+        disableClientWebpackPlugin: true,
+      },
+    }),
+    withBundleAnalyzer,
+  ],
+  {
+    distDir: 'build',
   },
-  distDir: 'build',
-}
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports)
+)
